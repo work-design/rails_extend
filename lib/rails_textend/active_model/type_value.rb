42a14f38e2dcfaff26c3114b8aca8830265f0c56
qinmingyuan <mingyuan0715@foxmail.com>
2021-09-06 20:42:48 +0800

    rename some module name

--- lib/rails_textend/active_model/type_value.rb
+++ lib/rails_textend/active_model/type_value.rb
@@ -4,6 +4,7 @@ module RailsExtend::ActiveModel
   module TypeValue
     attr_reader :options
 
+    # 让 attribute 方法支持更多选项
     def initialize(precision: nil, limit: nil, scale: nil, **options)
       @options = options
       super(precision: precision, limit: scale, scale: limit)
