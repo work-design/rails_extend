02e059c621f81fa0f22f2610b8782c4d5da03e05
qinmingyuan <mingyuan0715@foxmail.com>
2021-09-06 23:02:57 +0800

    moved attributes by type

--- lib/rails_extend/active_record/include.rb
+++ lib/rails_extend/active_record/include.rb
@@ -14,25 +14,6 @@ module RailsExtend::ActiveRecord
       self.class.base_class.name
     end
 
-    def attributes_with_type(except: ['id', 'created_at', 'updated_at'], only: [])
-      cols = {}
-      if only.present?
-        columns = attributes.slice(*only)
-      else
-        columns = attributes.except(*except)
-      end
-
-      columns.each do |key, value|
-        type = self.class.input_attributes_by_model[key] || self.class.attributes_by_belongs[key]
-        r = {}
-        r.merge! value: value, input_type: type[:input_type], **DefaultForm.config.mapping.fetch(type[:input_type], {})
-        r.merge! outer: type[:outer] if type.key? :outer
-        cols.merge! key => r
-      end
-
-      cols
-    end
-
     def as_full_json
       as_json(include: _reflections.keys)
     end
