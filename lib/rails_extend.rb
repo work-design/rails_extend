5405e1f2e2d9afc89bad8f28d6a30ea8450717de
qinmingyuan <mingyuan0715@foxmail.com>
2021-09-06 20:55:56 +0800

    add meta

--- lib/rails_extend.rb
+++ lib/rails_extend.rb
@@ -5,3 +5,8 @@ require 'rails_extend/core'
 
 require 'rails_extend/active_model'
 require 'rails_extend/type'  # 支持的 attribute type 扩展
+
+# Meta for Rails
+require 'rails_extend/routes'
+require 'rails_extend/models'
+require 'rails_extend/env'
