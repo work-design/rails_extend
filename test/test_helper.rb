100fd3dd7d7374282bb404b473030f2128e5b315
qinmingyuan <mingyuan0715@foxmail.com>
2021-09-08 17:06:29 +0800

    auto migrations

---
+++ test/test_helper.rb
@@ -0,0 +1,21 @@
+ENV['RAILS_ENV'] = 'test'
+require_relative 'dummy/config/environment'
+require 'rails/test_help'
+require 'minitest/mock'
+require 'minitest/hooks/test'
+
+Minitest.backtrace_filter = Minitest::BacktraceFilter.new
+
+class ActiveSupport::TestCase
+  self.fixture_path = File.expand_path('fixtures', __dir__)
+  self.file_fixture_path = fixture_path + '/files'
+  ActionDispatch::IntegrationTest.fixture_path = self.fixture_path
+  fixtures :all
+  #parallelize(workers: :number_of_processors)
+
+  def before_all
+    Roled::Busyness.sync
+    Roled::NameSpace.sync
+    Roled::Govern.sync
+  end
+end
