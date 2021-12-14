c41a2dbac04b62f9af113316390a547157a8e7cc
qinmingyuan <mingyuan0715@foxmail.com>
2021-12-14 15:26:11 +0800

    moved an method defined in rails 7

--- lib/rails_extend/core/pathname.rb
+++ lib/rails_extend/core/pathname.rb
@@ -1,11 +1,5 @@
 class Pathname
 
-  # like present? and presence
-  # if exist? return self
-  def existence
-    self if exist?
-  end
-
   # xx.html.erb => xx
   def without_extname
     suffix = basename.to_s.split('.')[0]
