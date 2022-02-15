ba29b31557e947197d222532ead94dade9d5f0a3
qinmingyuan <mingyuan0715@foxmail.com>
2022-02-15 17:47:48 +0800

    inhre

--- lib/rails_extend/action_controller/prepend.rb
+++ lib/rails_extend/action_controller/prepend.rb
@@ -5,11 +5,18 @@ module RailsExtend::ActionController
     # 支持在views/:controller 目录下，用 _action 开头的子目录进一步分组，会优先查找该目录下文件
     def _prefixes
       _action_name = (request&.params || {})['action']
-      if defined?(current_organ) && current_organ&.code.present?
-        ["#{current_organ.code}/views/#{controller_path}", "#{current_organ.code}/views", "#{controller_path}/_#{_action_name}"] + super
+
+      if self.class.superclass.action_methods.include?(_action_name)
+        pres = ["#{controller_path}/_#{_action_name}", "#{self.class.superclass.controller_path}/_#{_action_name}"] + super
       else
-        ["#{controller_path}/_#{_action_name}"] + super
+        pres = ["#{controller_path}/_#{_action_name}"] + super
       end
+
+      if defined?(current_organ) && current_organ&.code.present?
+        pres = ["#{current_organ.code}/views/#{controller_path}", "#{current_organ.code}/views"] + pres
+      end
+
+      pres
     end
 
   end
