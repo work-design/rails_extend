e1782fceae7e1474f28b241638ea84c7abbaa8c2
qinmingyuan <mingyuan0715@foxmail.com>
2022-04-02 22:51:33 +0800

    support engine

--- lib/rails_extend/exporter.rb
+++ lib/rails_extend/exporter.rb
@@ -11,7 +11,7 @@ module RailsExtend
       vite.append 'entry_paths', Rails.root.join('app/views').to_s
       vite.append 'entry_paths', Rails.root.join('app/entrypoints').to_s
       Rails::Engine.subclasses.each do |engine|
-        view_root = engine.root.join('app/views')
+        view_root = engine.root.join('app/assets', 'stylesheets')
         if view_root.directory?
           vite.append 'entry_paths', view_root.to_s
         end
@@ -19,6 +19,14 @@ module RailsExtend
         entrypoint_root = engine.root.join('app/assets', 'entrypoints')
         if entrypoint_root.directory?
           vite.append 'entry_paths', entrypoint_root.to_s
+        end
+
+        if engine.root.join('yarn.lock').exist? && view_root.directory?
+          Dir.chdir engine.root do
+            $stdout.puts "\e[35m  install \e[0m #{engine.root}"
+            system 'yarn install'
+          end
+          vite.append 'entry_paths', engine.root.join('node_modules').to_s
         end
       end
 
