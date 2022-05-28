9e56b86a7c24fc6728a86b09aa684186034b5310
qinmingyuan <mingyuan0715@foxmail.com>
2022-05-28 11:52:21 +0800

    get all superclas prefixes

--- lib/rails_extend/action_controller/prepend.rb
+++ lib/rails_extend/action_controller/prepend.rb
@@ -5,12 +5,14 @@ module RailsExtend::ActionController
     # 支持在views/:controller 目录下，用 _action 开头的子目录进一步分组，会优先查找该目录下文件
     def _prefixes
       _action_name = (request&.params || {})['action']
+      pres = ["#{controller_path}/_#{_action_name}", "#{controller_path}/_base"]
 
-      if self.class.superclass.action_methods.include?(_action_name)
-        pres = ["#{controller_path}/_#{_action_name}", "#{controller_path}/_base", "#{self.class.superclass.controller_path}/_#{_action_name}", "#{self.class.superclass.controller_path}/_base"] + super
-      else
-        pres = ["#{controller_path}/_#{_action_name}", "#{controller_path}/_base"] + super
+      _super_class = self.class.superclass
+      while _super_class.action_methods.include?(_action_name)
+        pres = pres + ["#{_super_class.controller_path}/_#{_action_name}", "#{_super_class.controller_path}/_base"]
+        _super_class = _super_class.superclass
       end
+      pres = pres + super
 
       if defined?(current_organ) && current_organ&.code.present?
         RailsExtend.config.override_prefixes.each do |pre|
