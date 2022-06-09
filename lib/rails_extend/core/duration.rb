8f6d93bf3dab6870691fb58968d00c3314687411
qinmingyuan <mingyuan0715@foxmail.com>
2022-06-09 15:43:41 +0800

    is slower

--- lib/rails_extend/core/duration.rb
+++ lib/rails_extend/core/duration.rb
@@ -3,10 +3,14 @@ class ActiveSupport::Duration
   def inspect
     return "#{value} #{I18n.t('duration.seconds')}" if @parts.empty?
 
-    @parts.
-    sort_by { |unit,  _ | PARTS.index(unit) }.
-    map     { |unit, val| "#{val} #{val == 1 ? I18n.t('duration')[unit] : I18n.t('durations')[unit]}" }.
-    to_sentence(locale: I18n.locale)
+    @parts.sort_by(&->(unit){ PARTS.index(unit[0]) }).map(&->(unit, val){ "#{val} #{val == 1 ? I18n.t('duration')[unit] : I18n.t('durations')[unit]}" }).to_sentence(locale: I18n.locale)
+  end
+
+  def in_all
+    all = {}
+    PARTS_IN_SECONDS.map do |i|
+
+    end
   end
 
 end
