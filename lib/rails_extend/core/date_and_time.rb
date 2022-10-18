cbecb14d1078e623ca406b9ec22c35edda5dfb08
qinmingyuan <mingyuan0715@foxmail.com>
2022-10-18 10:41:29 +0800

    chagne parts

--- lib/rails_extend/core/date_and_time.rb
+++ lib/rails_extend/core/date_and_time.rb
@@ -3,15 +3,14 @@ module RailsExtend
 
     # 用于 change 方法
     def parts
-      arr = to_a
       {
-        sec: arr[0],
-        min: arr[1],
-        hour: arr[2],
-        day: arr[3],
-        month: arr[4],
-        year: arr[5],
-        offset: arr[-1]
+        year: year,
+        month: month,
+        day: day,
+        hour: hour,
+        min: min,
+        sec: sec,
+        offset: formatted_offset
       }
     end
 
