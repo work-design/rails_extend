58bb78046d012e81af45e66871b3740df91946a1
qinmingyuan <mingyuan0715@foxmail.com>
2022-12-04 23:42:24 +0800

    more clear logic

--- lib/rails_extend/config.rb
+++ lib/rails_extend/config.rb
@@ -8,8 +8,15 @@ module RailsExtend #:nodoc:
   configure do |config|
     config.enum_key = ->(o, attribute){ "#{o.i18n_scope}.enum.#{o.base_class.model_name.i18n_key}.#{attribute}" }
     config.help_key = ->(o, attribute){ "#{o.i18n_scope}.help.#{o.base_class.model_name.i18n_key}.#{attribute}" }
-    config.ignore_models = []
-    config.override_prefixes = ['application']
+    config.ignore_models = [
+      'GoodJob::ActiveJobJob',
+      'GoodJob::Execution',
+      'GoodJob::Process',
+      'GoodJob::Job'
+    ]
+    config.override_prefixes = [
+      'application'
+    ]
     config.quiet_logs = [
       '/rails/active_storage',
       '/images',
