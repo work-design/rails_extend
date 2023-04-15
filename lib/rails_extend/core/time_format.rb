42502de591b54d7c1a7140eda6ec508229185fa0
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-15 18:32:31 +0800

    use chinese

--- lib/rails_extend/core/time_format.rb
+++ lib/rails_extend/core/time_format.rb
@@ -16,10 +16,13 @@ Time::DATE_FORMATS[:month] = ->(time) {
   I18n.t('date.month_names')[time.month]
 }
 
+Date::DATE_FORMATS[:month_and_day] = '%m-%d'
 Date::DATE_FORMATS[:week] = ->(date) {
   I18n.t('date.day_names')[date.wday]
 }
 Date::DATE_FORMATS[:month] = ->(date) {
   I18n.t('date.month_names')[date.month]
 }
-Date::DATE_FORMATS[:month_and_day] = '%m-%d'
+Date::DATE_FORMATS[:month_day] = ->(date) {
+  "#{date.month}月#{date.mday}日"
+}
