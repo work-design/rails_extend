071b6607366c07f381fcc1c6e49879ecc3e9f672
qinmingyuan <mingyuan0715@foxmail.com>
2023-10-15 18:08:12 +0800

    name

--- lib/rails_extend/action_controller/prepend.rb
+++ lib/rails_extend/action_controller/prepend.rb
@@ -6,13 +6,14 @@ module RailsExtend::ActionController
       # 支持在 views/:controller 目录下以 _:action 开头的子目录进一步分组，会优先查找该目录下文件
       # 支持在 views/:controller 目录下以 _base 开头的通用分组子目录
       pres = ["#{controller_path}/_#{params['action']}", "#{controller_path}/_base"]
-      names = [params[:namespace]]
+      names = ["#{params[:business]}/#{params[:namespace]}"]
 
       super_class = self.class.superclass
       # 同名 controller, 向上级追溯
       while RailsExtend::Routes.find_actions(super_class.controller_path).include?(params['action'])
         pres.concat ["#{super_class.controller_path}/_#{params['action']}", "#{super_class.controller_path}/_base"]
-        names.append RailsExtend::Routes.controller_paths.dig(super_class.controller_path, :namespace)
+        x = RailsExtend::Routes.controller_paths.dig(super_class.controller_path)
+        names.append "#{x[:business]}/#{x[:namespace]}"
         super_class = super_class.superclass
       end
       # 可以在 controller 中定义 _prefixes 方法
@@ -26,13 +27,11 @@ module RailsExtend::ActionController
 
       names.compact_blank!
       if names.size >= 2
-        names[0...-1].zip(names[1..-1]).each do |before, after|
-          if params[:business]
-            base_con = "#{params[:business]}/#{before}/base"
-            if pres.exclude?(base_con)
-              r = pres.find_index(&->(i){ i == "#{params[:business]}/#{after}/base" })
-              pres.insert(r, base_con) if r
-            end
+        names[0...-1].zip(names[1..-1]).reverse_each do |before, after|
+          base_con = "#{before}/base"
+          if pres.exclude?(base_con)
+            r = pres.find_index(&->(i){ i == "#{after}/base" })
+            pres.insert(r, base_con) if r
           end
 
           if pres.exclude?(before)
