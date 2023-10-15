7b0fe8de6cce88ff10a5e5afc461db43b1fdf608
qinmingyuan <mingyuan0715@foxmail.com>
2023-10-15 17:47:49 +0800

    namespace

--- lib/rails_extend/action_controller/prepend.rb
+++ lib/rails_extend/action_controller/prepend.rb
@@ -6,11 +6,13 @@ module RailsExtend::ActionController
       # 支持在 views/:controller 目录下以 _:action 开头的子目录进一步分组，会优先查找该目录下文件
       # 支持在 views/:controller 目录下以 _base 开头的通用分组子目录
       pres = ["#{controller_path}/_#{params['action']}", "#{controller_path}/_base"]
+      names = [params[:namespace]]
 
       super_class = self.class.superclass
       # 同名 controller, 向上级追溯
       while RailsExtend::Routes.find_actions(super_class.controller_path).include?(params['action'])
-        pres = pres + ["#{super_class.controller_path}/_#{params['action']}", "#{super_class.controller_path}/_base"]
+        pres.concat ["#{super_class.controller_path}/_#{params['action']}", "#{super_class.controller_path}/_base"]
+        names.append RailsExtend::Routes.controller_paths.dig(super_class.controller_path, :namespace)
         super_class = super_class.superclass
       end
       # 可以在 controller 中定义 _prefixes 方法
@@ -22,15 +24,21 @@ module RailsExtend::ActionController
       end
       pres += super
 
-      if params['namespace']
-        base_con = "#{params['business']}/#{params['namespace']}/base"
-        if params['business'] && pres.exclude?(base_con)
-          r = pres.find_index(&->(i){ i.exclude?('/') })
-        end
+      names.compact_blank!
+      if names.size >= 2
+        names[0...-1].zip(names[1..-1]).each do |before, after|
+          if params[:business]
+            base_con = "#{params[:business]}/#{before}/base"
+            if pres.exclude?(base_con)
+              r = pres.find_index(&->(i){ i == "#{params[:business]}/#{after}/base" })
+              pres.insert(r, base_con) if r
+            end
+          end
 
-        if pres.exclude?(params['namespace'])
-          r = pres.find_index(&->(i){ i.exclude?('/') })
-          pres.insert(r, params[:namespace]) if r
+          if pres.exclude?(before)
+            r = pres.find_index(&->(i){ i == after })
+            pres.insert(r, before) if r
+          end
         end
       end
 
