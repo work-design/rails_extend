4d8fc56bcc723bee4072da48da81b29756f365df
qinmingyuan <mingyuan0715@foxmail.com>
2023-11-04 16:17:35 +0800

    extend attached

--- lib/rails_extend/active_storage.rb
+++ lib/rails_extend/active_storage.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
-
+require 'rails_extend/active_storage/activestorage_attached'
 require 'rails_extend/active_storage/attachment_include'
 require 'rails_extend/active_storage/attachment_prepend'
 require 'rails_extend/active_storage/blob_prepend'
 require 'rails_extend/active_storage/variant'
 require 'rails_extend/active_storage/video_response'
-#require 'rails_extend/active_storage/activestorage_attached'
