6362861262af0c6035ad0ff43ba760f1777d8201
qinmingyuan <mingyuan0715@foxmail.com>
2023-11-08 17:11:05 +0800

    add new

--- lib/rails_extend/action_dispatch/mapper.rb
+++ lib/rails_extend/action_dispatch/mapper.rb
@@ -2,6 +2,9 @@ module RailsExtend::ActionDispatch
   module Mapper
 
     def set_member_mappings_for_resource
+      new do
+        post :new
+      end
       member do
         post :actions
       end
