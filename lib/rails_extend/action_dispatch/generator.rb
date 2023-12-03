ec071bb0cf74a0350f44fe3a24773face8b4a339
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-03 16:51:54 +0800

    simple

--- lib/rails_extend/action_dispatch/generator.rb
+++ lib/rails_extend/action_dispatch/generator.rb
@@ -5,6 +5,11 @@ module RailsExtend::ActionDispatch
     def use_relative_controller!
       _controller = @options[:controller]
 
+      if !named_route && different_controller? && !controller.start_with?('/') && controller.count('/') > 0
+        path = (current_controller.split('/')[0..-2] << controller).join('/')
+        return @options[:controller] = path if RailsExtend::Routes._controllers.key?(path)
+      end
+
       super
 
       return if RailsExtend::Routes._controllers.key?(@options[:controller])
