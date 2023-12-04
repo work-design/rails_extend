639eb9194fffc043dc9b8903eab532fc4814285c
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-04 14:01:28 +0800

    next is base

--- lib/rails_extend/action_controller/prepend.rb
+++ lib/rails_extend/action_controller/prepend.rb
@@ -5,20 +5,22 @@ module RailsExtend::ActionController
     def _prefixes
       # 支持在 views/:controller 目录下以 _:action 开头的子目录进一步分组，会优先查找该目录下文件
       # 支持在 views/:controller 目录下以 _base 开头的通用分组子目录
-      pres = ["#{controller_path}/_#{params['action']}", "#{controller_path}/_base"]
+      pres = ["#{controller_path}/_#{params['action']}"]
       names = ["#{params[:business]}/#{params[:namespace]}"]
       namespaces = [params[:namespace]]
 
       super_class = self.class.superclass
       # 同名 controller, 向上级追溯
       while RailsExtend::Routes.find_actions(super_class.controller_path).include?(params['action'])
-        pres.concat ["#{super_class.controller_path}/_#{params['action']}", "#{super_class.controller_path}/_base"]
+        pres << "#{super_class.controller_path}/_#{params['action']}"
         x = RailsExtend::Routes.controller_paths.dig(super_class.controller_path)
         _namespace = x[:namespace].presence || 'application'
         names.append "#{x[:business]}/#{_namespace}"
         namespaces.append _namespace unless namespaces.include?(_namespace)
         super_class = super_class.superclass
       end
+
+      pres.concat pres.map(&->(i){ i.sub(/_#{params[:action]}$/, '_base') })
       # 可以在 controller 中定义 _prefixes 方法
       # super do |pres|
       #   pres + ['xx']
